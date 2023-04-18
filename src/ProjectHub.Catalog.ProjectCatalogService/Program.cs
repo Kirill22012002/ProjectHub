9ee29f21dcd499b3321aac28a1ea5a24e0c536dd
Kirill22022002 <perepechkin_2002@mail.ru>
2023-04-18 10:57:23 +0300

    added bearer token to swagger

--- src/ProjectHub.Catalog.ProjectCatalogService/Program.cs
+++ src/ProjectHub.Catalog.ProjectCatalogService/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 using ProjectHub.Catalog.ProjectCatalogService;
 using ProjectHub.Catalog.ProjectCatalogService.Data;
 using ProjectHub.Catalog.ProjectCatalogService.Repositories.Implementations;
@@ -16,19 +17,54 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Enter 'Bearer' [space] and then your token in the text input below.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+                
+            },
+            new List<string>()  
+        }   
+    });
+});
 
 builder.Services.AddAuthentication(config =>
-{
-    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
+    {
+        config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
     .AddJwtBearer("Bearer", options =>
     {
-        options.Authority = "http://localhost:43056/";
-        options.Audience = "WebAPI";
+        
+        // options.Authority = "http://localhost:43056/";
+        options.Authority = "http://ec2-54-203-48-186.us-west-2.compute.amazonaws.com:5216";
+        
+        // options.Audience = "WebAPI";
+        options.Audience = "TownSend_Backend_Resource";
+        
         options.RequireHttpsMetadata = false;
     });
+
+builder.Services.AddHttpClient();
 
 builder.Services.AddDbContext<ProjectCatalogDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DockerLocalConnString")));
